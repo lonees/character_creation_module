812602f6d2285fdc7af97cfe1c44d7b2dcc2799f
Иван Баклаг <ivan.baklag@gmail.com>
2023-02-08 15:07:31 +0800

    add module graphic_arts and docstrings for main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from random import randint
-from typing import Union
+
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Выводит сообщение о количестве аттаке персонажа
+       в зависимости от его класса."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный'
                 f' {5 + randint(3, 5)}')
@@ -16,6 +19,8 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Выводит сообщение о количестве заблокированного персонажем урона
+       в зависимости от его класса."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -26,6 +31,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Выводит сообщение о применении специального умения класса персонажа."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение «Выносливость'
                 ' {80 + 25}»')
@@ -37,6 +43,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Запускает процесс тренировки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -48,7 +55,7 @@ def start_training(char_name: str, char_class: str) -> str:
           ' defence — чтобы блокировать атаку противника'
           ' или special — чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd: Union[None, str] = None
+    cmd: str = ''
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -61,8 +68,9 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
-    approve_choice: Union[None, str] = None
-    char_class: str = 'None'
+    """Возвращает выбранный игроком класс персонажа."""
+    approve_choice: str = ''
+    char_class: str = ''
     while approve_choice != 'y':
         char_class = input('Введи название персонажа, за которого хочешь'
                            ' играть: Воитель — warrior, Маг — mage,'
@@ -82,7 +90,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -92,6 +101,3 @@ def main() -> None:
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
